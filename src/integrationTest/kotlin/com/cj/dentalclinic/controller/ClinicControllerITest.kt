8705f43d18b7b00b070d10c2ad2bc169d0990d0d
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-02 21:49:44 +0530

    add mockk dependency add service layer fix test cases refactor the code

--- src/integrationTest/kotlin/com/cj/dentalclinic/controller/ClinicControllerITest.kt
+++ src/integrationTest/kotlin/com/cj/dentalclinic/controller/ClinicControllerITest.kt
@@ -1,21 +1,31 @@
 package com.cj.dentalclinic.controller
 
+import com.cj.dentalclinic.service.ClinicService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.context.annotation.Import
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
 const val CLINIC_BASE_URI = "/api/v1/clinics"
 
 @WebMvcTest(ClinicController::class)
+@Import(ClinicService::class)
 internal class ClinicControllerITest(@Autowired val mockMvc: MockMvc) {
 
   @Test
-  fun `clinics endpoint should return json array of all the clinics with 200 status code`() {
+  fun `should return 200 status code`() {
     mockMvc.get(CLINIC_BASE_URI)
       .andExpect {
         status { isOk() }
+      }
+  }
+
+  @Test
+  fun `should return json array of all the clinics`() {
+    mockMvc.get(CLINIC_BASE_URI)
+      .andExpect {
         content { json("""[{"id":1,"name":"Sharda Dental Clinic"},{"id":2,"name":"Smart Dental Clinic"}]""") }
       }
   }
--- src/main/kotlin/com/cj/dentalclinic/controller/ClinicController.kt
+++ src/main/kotlin/com/cj/dentalclinic/controller/ClinicController.kt
@@ -1,17 +1,17 @@
 package com.cj.dentalclinic.controller
 
 import com.cj.dentalclinic.dto.Clinic
+import com.cj.dentalclinic.service.ClinicService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/clinics")
-class ClinicController {
+class ClinicController(@Autowired private val clinicService: ClinicService) {
 
   @GetMapping
-  fun getAllClinics(): List<Clinic> {
-    return listOf(Clinic(1, "Sharda Dental Clinic"), Clinic(2, "Smart Dental Clinic"))
-  }
+  fun getAllClinics(): List<Clinic> = clinicService.getAllClinics()
 
 }
