d58b30cc2c3f9ef928dfd23066411f7516769a4a
Chetan Chilhate <cchilhate@medline.com>
2022-06-03 09:24:41 +0530

    separate entity & dto classes

--- src/integrationTest/kotlin/com/cj/dentalclinic/controller/ClinicDtoControllerITest.kt
+++ src/integrationTest/kotlin/com/cj/dentalclinic/controller/ClinicDtoControllerITest.kt
@@ -1,6 +1,6 @@
 package com.cj.dentalclinic.controller
 
-import com.cj.dentalclinic.dto.ClinicDto
+import com.cj.dentalclinic.entity.Clinic
 import com.cj.dentalclinic.repository.ClinicRepository
 import com.cj.dentalclinic.service.ClinicService
 import com.ninjasquad.springmockk.MockkBean
@@ -25,8 +25,8 @@ internal class ClinicDtoControllerITest(@Autowired val mockMvc: MockMvc) {
   @BeforeEach
   fun setup() {
     every { clinicRepository.findAll() } returns listOf(
-      ClinicDto(1, "Sharda Dental Clinic"),
-      ClinicDto(2, "Smart Dental Clinic")
+      Clinic(1, "Sharda Dental Clinic"),
+      Clinic(2, "Smart Dental Clinic")
     )
   }
 
