b816a3b6ed3462053ee4528b8257500f38332dfd
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-03 08:45:46 +0530

    rename Clinic to ClinicDto

--- src/main/kotlin/com/cj/dentalclinic/repository/ClinicRepository.kt
+++ src/main/kotlin/com/cj/dentalclinic/repository/ClinicRepository.kt
@@ -1,8 +1,8 @@
 package com.cj.dentalclinic.repository
 
-import com.cj.dentalclinic.dto.Clinic
+import com.cj.dentalclinic.dto.ClinicDto
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface ClinicRepository : JpaRepository<Clinic, Int>
+interface ClinicRepository : JpaRepository<ClinicDto, Int>
--- src/main/kotlin/com/cj/dentalclinic/service/ClinicService.kt
+++ src/main/kotlin/com/cj/dentalclinic/service/ClinicService.kt
@@ -1,6 +1,6 @@
 package com.cj.dentalclinic.service
 
-import com.cj.dentalclinic.dto.Clinic
+import com.cj.dentalclinic.dto.ClinicDto
 import com.cj.dentalclinic.repository.ClinicRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -10,6 +10,6 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional
 class ClinicService(@Autowired val clinicRepository: ClinicRepository) {
 
-  fun getAllClinics(): List<Clinic> = clinicRepository.findAll()
+  fun getAllClinics(): List<ClinicDto> = clinicRepository.findAll()
 
 }
