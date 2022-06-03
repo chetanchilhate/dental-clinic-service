f12d8032f955f382e4a2d49fd233921c82695296
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-03 23:59:20 +0530

    ClinicController.kt should throw ResourceNotFoundException when no Clinic found with given id

--- src/test/kotlin/com/cj/dentalclinic/controller/ClinicDtoControllerTest.kt
+++ src/test/kotlin/com/cj/dentalclinic/controller/ClinicDtoControllerTest.kt
@@ -1,10 +1,12 @@
 package com.cj.dentalclinic.controller
 
 import com.cj.dentalclinic.dto.ClinicDto
+import com.cj.dentalclinic.exception.ResourceNotFoundException
 import com.cj.dentalclinic.service.ClinicService
 import io.mockk.mockk
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
 internal class ClinicDtoControllerTest {
@@ -33,6 +35,16 @@ internal class ClinicDtoControllerTest {
 
     assertThat(clinicDto.name).isNotBlank
 
+  }
+
+  @Test
+  fun `should throw ResourceNotFoundException when no Clinic found with given id`() {
+
+    val clinicId = 4
+
+    assertThatThrownBy { clinicController.getClinicById(clinicId) }
+      .isInstanceOf(ResourceNotFoundException::class.java)
+      .hasMessage("No Clinic found with given id : $clinicId")
   }
 
 }
