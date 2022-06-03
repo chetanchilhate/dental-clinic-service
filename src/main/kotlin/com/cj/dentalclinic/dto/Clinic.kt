6ded1a0ae3718f9d6e28c5d49d38bc6105419e55
Chetan Chilhate <cchilhate@medline.com>
2022-06-03 08:11:07 +0530

    convert Clinic pojo to entity add schema & data sql files to create table & data

--- src/main/kotlin/com/cj/dentalclinic/dto/Clinic.kt
+++ src/main/kotlin/com/cj/dentalclinic/dto/Clinic.kt
@@ -1,3 +1,23 @@
 package com.cj.dentalclinic.dto
 
-data class Clinic(val id: Int, val name: String)
+import org.hibernate.Hibernate
+import javax.persistence.*
+
+@Entity
+@Table(name = "t_clinic")
+data class Clinic(@Id  @GeneratedValue(strategy = GenerationType.AUTO) var id: Int?, val name: String) {
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+    other as Clinic
+
+    return id != null && id == other.id
+  }
+
+  override fun hashCode(): Int = javaClass.hashCode()
+
+  @Override
+  override fun toString(): String {
+    return this::class.simpleName + "(id = $id, name = $name)"
+  }
+}
---
+++ src/main/resources/sql/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS t_clinic;
+
+CREATE TABLE t_clinic
+(
+    id   int          NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
