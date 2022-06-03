73a46b65f093ce53fe670280504e1e962781cf03
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-03 08:11:07 +0530

    convert Clinic pojo to entity add schema & data sql files to create table & data

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO t_clinic (id, name) VALUES (1, 'Sharda Dental Clinic');
+INSERT INTO t_clinic (id, name) VALUES (2, 'Smart Dental Clinic');
+INSERT INTO t_clinic (id, name) VALUES (3, 'Sonal Dental Clinic');
