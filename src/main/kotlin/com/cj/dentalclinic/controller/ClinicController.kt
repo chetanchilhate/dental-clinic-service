290dad7f00df8f833165031d90db64b330e38960
Chetan Chilhate <cchilhate@medline.com>
2022-06-03 23:16:07 +0530

    add method in ClinicController.kt to return ClinicDto with give id

--- src/main/kotlin/com/cj/dentalclinic/controller/ClinicController.kt
+++ src/main/kotlin/com/cj/dentalclinic/controller/ClinicController.kt
@@ -1,9 +1,11 @@
 package com.cj.dentalclinic.controller
 
 import com.cj.dentalclinic.dto.ClinicDto
+import com.cj.dentalclinic.entity.Clinic
 import com.cj.dentalclinic.service.ClinicService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,4 +16,12 @@ class ClinicController(@Autowired private val clinicService: ClinicService) {
   @GetMapping
   fun getAllClinics(): List<ClinicDto> = clinicService.getAllClinics()
 
+  @GetMapping("/{id}")
+  fun getClinicById(@PathVariable("id") clinicId: Int): ClinicDto =
+    listOf(Clinic(1, "Sharda Dental Clinic"), Clinic(2, "Smart Dental Clinic"))
+      .stream()
+      .filter { t -> t.id == clinicId }
+      .map { t -> ClinicDto(t) }
+      .findFirst()
+      .orElse(ClinicDto(0, ""))
 }
--- src/test/kotlin/com/cj/dentalclinic/controller/ClinicDtoControllerTest.kt
+++ src/test/kotlin/com/cj/dentalclinic/controller/ClinicDtoControllerTest.kt
@@ -1,8 +1,10 @@
 package com.cj.dentalclinic.controller
 
+import com.cj.dentalclinic.dto.ClinicDto
 import com.cj.dentalclinic.service.ClinicService
 import io.mockk.mockk
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 internal class ClinicDtoControllerTest {
@@ -17,6 +19,19 @@ internal class ClinicDtoControllerTest {
     clinicController.getAllClinics()
 
     verify(exactly = 1) { clinicService.getAllClinics() }
+
+  }
+
+  @Test
+  fun `should return Clinic with given id with a name`() {
+
+    val clinicId = 2
+
+    val clinicDto: ClinicDto =  clinicController.getClinicById(clinicId)
+
+    assertThat(clinicDto.id).isEqualTo(clinicId)
+
+    assertThat(clinicDto.name).isNotBlank
 
   }
 
