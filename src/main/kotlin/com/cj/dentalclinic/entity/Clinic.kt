276e9c748456761af3c8473a76e0467661b6f4c2
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-04 19:45:05 +0530

    add generation type for Clinic primary key if id is null compare name for data equality add constructor to map clinic dto to entity format Clinic toString

--- src/main/kotlin/com/cj/dentalclinic/entity/Clinic.kt
+++ src/main/kotlin/com/cj/dentalclinic/entity/Clinic.kt
@@ -1,27 +1,32 @@
 package com.cj.dentalclinic.entity
 
+import com.cj.dentalclinic.dto.ClinicDto
 import org.hibernate.Hibernate
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType.IDENTITY
 import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "t_clinic")
-data class Clinic(@Id val id: Int?, val name: String) {
+data class Clinic(@Id @GeneratedValue(strategy = IDENTITY) val id: Int? = null, val name: String) {
+
+  constructor(clinicDto: ClinicDto) : this(clinicDto.id, clinicDto.name)
 
   override fun equals(other: Any?): Boolean {
     if (this === other) return true
     if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
     other as Clinic
 
-    return id != null && id == other.id
+    return if (id != null) id == other.id else name == other.name
   }
 
   override fun hashCode(): Int = javaClass.hashCode()
 
   @Override
   override fun toString(): String {
-    return this::class.simpleName + "(id = $id, name = $name)"
+    return this::class.simpleName + "(id = $id , name = $name )"
   }
 
 }
