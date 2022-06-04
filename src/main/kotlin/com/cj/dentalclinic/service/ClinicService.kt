1849be8785c8f0e396a0633ea090e293ae3cb1db
Chetan Chilhate <cchilhate@medline.com>
2022-06-04 15:30:53 +0530

    add ClinicRepository call in ClinicService for findById refactor & cleanup test cases

--- src/main/kotlin/com/cj/dentalclinic/service/ClinicService.kt
+++ src/main/kotlin/com/cj/dentalclinic/service/ClinicService.kt
@@ -1,7 +1,6 @@
 package com.cj.dentalclinic.service
 
 import com.cj.dentalclinic.dto.ClinicDto
-import com.cj.dentalclinic.entity.Clinic
 import com.cj.dentalclinic.exception.ResourceNotFoundException
 import com.cj.dentalclinic.repository.ClinicRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -18,12 +17,9 @@ class ClinicService(@Autowired val clinicRepository: ClinicRepository) {
     .map { t -> ClinicDto(t) }
     .toList()
 
-  fun getClinicById(clinicId: Int): ClinicDto =
-    listOf(Clinic(1, "Sharda Dental Clinic"), Clinic(2, "Smart Dental Clinic"))
-      .stream()
-      .filter { t -> t.id == clinicId }
-      .map { t -> ClinicDto(t) }
-      .findFirst()
-      .orElseThrow { ResourceNotFoundException("Clinic", clinicId) }
+  fun getClinicById(clinicId: Int): ClinicDto = clinicRepository
+    .findById(clinicId)
+    .map { t -> ClinicDto(t) }
+    .orElseThrow { ResourceNotFoundException("Clinic", clinicId) }
 
 }
--- src/test/kotlin/com/cj/dentalclinic/service/ClinicServiceTest.kt
+++ src/test/kotlin/com/cj/dentalclinic/service/ClinicServiceTest.kt
@@ -1,11 +1,16 @@
 package com.cj.dentalclinic.service
 
+import com.cj.dentalclinic.entity.Clinic
 import com.cj.dentalclinic.exception.ResourceNotFoundException
 import com.cj.dentalclinic.repository.ClinicRepository
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.assertj.core.api.Assertions
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.*
+import java.util.Optional.empty
+import java.util.Optional.of
 
 internal class ClinicServiceTest {
 
@@ -14,7 +19,7 @@ internal class ClinicServiceTest {
   private val clinicService = ClinicService(clinicRepository)
 
   @Test
-  fun `should call ClinicService to getAll clinics`() {
+  fun `should call ClinicRepository to findAll clinics`() {
 
     clinicService.getAllClinics()
 
@@ -22,27 +27,52 @@ internal class ClinicServiceTest {
 
   }
 
-  @Test
-  fun `should return Clinic with given id with a name`() {
+  @Nested
+  @DisplayName("getClinicById(clinicId: Int)")
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  inner class GetClinicById {
 
-    val clinicId = 2
+    private val clinicIdWithClinic = 2
 
-    val clinicDto = clinicService.getClinicById(clinicId)
+    private val clinicIdWithOutClinic = 4
 
-    Assertions.assertThat(clinicDto.id).isEqualTo(clinicId)
+    @BeforeEach
+    fun setup() {
 
-    Assertions.assertThat(clinicDto.name).isNotBlank
+      every { clinicRepository.findById(clinicIdWithClinic) } returns of(Clinic(clinicIdWithClinic, "Smart Dental Clinic"))
 
-  }
+      every { clinicRepository.findById(clinicIdWithOutClinic) } returns empty()
 
-  @Test
-  fun `should throw ResourceNotFoundException when no Clinic found with given id`() {
+    }
 
-    val clinicId = 4
+    @Test
+    fun `should return Clinic with given id with a name`() {
 
-    Assertions.assertThatThrownBy { clinicService.getClinicById(clinicId) }
-      .isInstanceOf(ResourceNotFoundException::class.java)
-      .hasMessage("No Clinic found with id : $clinicId")
+      val clinicDto = clinicService.getClinicById(clinicIdWithClinic)
+
+      assertThat(clinicDto.id).isEqualTo(clinicIdWithClinic)
+
+      assertThat(clinicDto.name).isNotBlank
+
+    }
+
+    @Test
+    fun `should throw ResourceNotFoundException when no Clinic found with given id`() {
+
+      assertThatThrownBy { clinicService.getClinicById(clinicIdWithOutClinic) }
+        .isInstanceOf(ResourceNotFoundException::class.java)
+        .hasMessage("No Clinic found with id : $clinicIdWithOutClinic")
+    }
+
+    @Test
+    fun `should call ClinicRepository to find clinic by id`() {
+
+      clinicService.getClinicById(clinicIdWithClinic)
+
+      verify(exactly = 1) { clinicRepository.findById(clinicIdWithClinic) }
+
+    }
+
   }
 
 }
