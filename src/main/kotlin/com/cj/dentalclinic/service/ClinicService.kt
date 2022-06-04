20e54eda57715fbe1b5a353da7e4527b9c2615ef
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-04 09:51:31 +0530

    add implementation for ClinicService getClinicById and add test cases when Clinic is present & not present

--- src/main/kotlin/com/cj/dentalclinic/service/ClinicService.kt
+++ src/main/kotlin/com/cj/dentalclinic/service/ClinicService.kt
@@ -1,6 +1,8 @@
 package com.cj.dentalclinic.service
 
 import com.cj.dentalclinic.dto.ClinicDto
+import com.cj.dentalclinic.entity.Clinic
+import com.cj.dentalclinic.exception.ResourceNotFoundException
 import com.cj.dentalclinic.repository.ClinicRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -16,6 +18,12 @@ class ClinicService(@Autowired val clinicRepository: ClinicRepository) {
     .map { t -> ClinicDto(t) }
     .toList()
 
-  fun getClinicById(clinicId: Int): ClinicDto = ClinicDto(0, "")
+  fun getClinicById(clinicId: Int): ClinicDto =
+    listOf(Clinic(1, "Sharda Dental Clinic"), Clinic(2, "Smart Dental Clinic"))
+      .stream()
+      .filter { t -> t.id == clinicId }
+      .map { t -> ClinicDto(t) }
+      .findFirst()
+      .orElseThrow { ResourceNotFoundException("Clinic", clinicId) }
 
 }
