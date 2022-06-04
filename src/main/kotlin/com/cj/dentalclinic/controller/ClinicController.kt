e60830408876553c64227b19eff45bcc589a40f5
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-04 09:34:25 +0530

    add ClinicService call in ClinicController for get Clinic by id

--- src/main/kotlin/com/cj/dentalclinic/controller/ClinicController.kt
+++ src/main/kotlin/com/cj/dentalclinic/controller/ClinicController.kt
@@ -1,8 +1,6 @@
 package com.cj.dentalclinic.controller
 
 import com.cj.dentalclinic.dto.ClinicDto
-import com.cj.dentalclinic.entity.Clinic
-import com.cj.dentalclinic.exception.ResourceNotFoundException
 import com.cj.dentalclinic.service.ClinicService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,11 +16,5 @@ class ClinicController(@Autowired private val clinicService: ClinicService) {
   fun getAllClinics(): List<ClinicDto> = clinicService.getAllClinics()
 
   @GetMapping("/{id}")
-  fun getClinicById(@PathVariable("id") clinicId: Int): ClinicDto =
-    listOf(Clinic(1, "Sharda Dental Clinic"), Clinic(2, "Smart Dental Clinic"))
-      .stream()
-      .filter { t -> t.id == clinicId }
-      .map { t -> ClinicDto(t) }
-      .findFirst()
-      .orElseThrow { ResourceNotFoundException("Clinic", clinicId) }
+  fun getClinicById(@PathVariable("id") clinicId: Int): ClinicDto = clinicService.getClinicById(clinicId)
 }
