0306d354ea2f34c92f89e007827307560b5c3e13
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-06 22:22:39 +0530

    add JPQL to get all the treatment by clinicId and get treatment with clinic by treatment id

--- src/main/kotlin/com/cj/dentalclinic/repository/TreatmentRepository.kt
+++ src/main/kotlin/com/cj/dentalclinic/repository/TreatmentRepository.kt
@@ -2,16 +2,17 @@ package com.cj.dentalclinic.repository
 
 import com.cj.dentalclinic.entity.Treatment
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.stereotype.Repository
 import java.util.Optional
 
+@Repository
 interface TreatmentRepository : JpaRepository<Treatment, Int> {
 
-  fun findAllByClinicId(clinicId: Int): List<Treatment> {
-    TODO("Not yet implemented")
-  }
+  @Query("SELECT t FROM Treatment t WHERE t.clinic.id = ?1")
+  fun findAllByClinicId(clinicId: Int): List<Treatment>
 
-  fun findTreatmentById(treatmentId: Int): Optional<Treatment> {
-    TODO("Not yet implemented")
-  }
+  @Query("SELECT t FROM Treatment t JOIN FETCH t.clinic WHERE t.id = ?1")
+  fun findTreatmentById(treatmentId: Int): Optional<Treatment>
 
 }
