1e0e0bdc1a3ac94717b612c22d82354bcaae2776
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-09 21:38:36 +0530

    update table name to plural add doctors table

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,6 +1,9 @@
-INSERT INTO t_clinic (id, name) VALUES (1, 'Sharda Dental Clinic');
-INSERT INTO t_clinic (id, name) VALUES (2, 'Smart Dental Clinic');
-INSERT INTO t_clinic (id, name) VALUES (3, 'Sonal Dental Clinic');
+INSERT INTO t_clinics (id, name) VALUES (1, 'Sharda Dental Clinic');
+INSERT INTO t_clinics (id, name) VALUES (2, 'Smart Dental Clinic');
+INSERT INTO t_clinics (id, name) VALUES (3, 'Sonal Dental Clinic');
 
-INSERT INTO t_treatment (id, name, fee, clinic_id) VALUES (1, 'Regular Checkup', 100.00, 1);
-INSERT INTO t_treatment (id, name, fee, clinic_id) VALUES (2, 'Root Canal', 2500.00, 1);
+INSERT INTO t_treatments (id, name, fee, clinic_id) VALUES (1, 'Regular Checkup', 100.00, 1);
+INSERT INTO t_treatments (id, name, fee, clinic_id) VALUES (2, 'Root Canal', 2500.00, 1);
+
+INSERT INTO t_doctors (email, first_name, last_name, qualification, clinic_id)
+VALUES ('atul@gmail.com', 'Atul', 'Kenjale', 'BDS', 1);
--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,8 +1,10 @@
-DROP TABLE IF EXISTS t_treatment;
+DROP TABLE IF EXISTS t_doctors;
 
-DROP TABLE IF EXISTS t_clinic;
+DROP TABLE IF EXISTS t_treatments;
 
-CREATE TABLE t_clinic
+DROP TABLE IF EXISTS t_clinics;
+
+CREATE TABLE t_clinics
 (
     id   INT          NOT NULL AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
@@ -10,7 +12,7 @@ CREATE TABLE t_clinic
 );
 
 
-CREATE TABLE t_treatment
+CREATE TABLE t_treatments
 (
     id        INT          NOT NULL AUTO_INCREMENT,
     name      VARCHAR(255) NOT NULL,
@@ -18,5 +20,19 @@ CREATE TABLE t_treatment
     clinic_id INT          NOT NULL,
     PRIMARY KEY (id),
     KEY fk_clinic_treatment (clinic_id),
-    CONSTRAINT fk_clinic_treatment FOREIGN KEY (clinic_id) REFERENCES t_clinic (id)
+    CONSTRAINT fk_clinic_treatment FOREIGN KEY (clinic_id) REFERENCES t_clinics (id)
+);
+
+CREATE TABLE t_doctors
+(
+    id            INT          NOT NULL AUTO_INCREMENT,
+    email         VARCHAR(100) NOT NULL,
+    first_name    VARCHAR(100) NOT NULL,
+    middle_name   VARCHAR(100) DEFAULT '',
+    last_name     VARCHAR(100) NOT NULL,
+    qualification VARCHAR(255) NOT NULL,
+    clinic_id     INT          NOT NULL,
+    PRIMARY KEY (id),
+    KEY fk_clinic_doctor (clinic_id),
+    CONSTRAINT fk_clinic_doctor FOREIGN KEY (clinic_id) REFERENCES t_clinics (id)
 );
