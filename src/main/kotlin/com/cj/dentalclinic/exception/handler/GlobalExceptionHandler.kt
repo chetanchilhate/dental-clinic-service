d3485d972b924543d454c041586c86f498fbd35d
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-10 20:48:52 +0530

    add validation exception handler

--- src/main/kotlin/com/cj/dentalclinic/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/cj/dentalclinic/exception/handler/GlobalExceptionHandler.kt
@@ -2,10 +2,13 @@ package com.cj.dentalclinic.exception.handler
 
 import com.cj.dentalclinic.exception.ResourceNotFoundException
 import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import java.util.stream.Collectors.joining
 
 data class ErrorResponse(val code: HttpStatus, val message: String?)
 
@@ -14,5 +17,20 @@ class GlobalExceptionHandler {
 
   @ResponseStatus(NOT_FOUND)
   @ExceptionHandler(ResourceNotFoundException::class)
-  fun handleResourceNotFoundException(exception: ResourceNotFoundException) = ErrorResponse(NOT_FOUND, exception.message)
+  fun handleResourceNotFoundException(exception: ResourceNotFoundException) =
+    ErrorResponse(NOT_FOUND, exception.message)
+
+  @ResponseStatus(BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException::class)
+  fun handleValidationExceptions(exception: MethodArgumentNotValidException): ErrorResponse {
+    val message = exception
+      .bindingResult
+      .allErrors
+      .stream()
+      .map { it.defaultMessage }
+      .collect(joining(", "))
+
+    return ErrorResponse(BAD_REQUEST, message)
+  }
+
 }
