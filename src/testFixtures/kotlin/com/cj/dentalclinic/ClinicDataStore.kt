204113cd006cda03614776063dd6bfc7ab6dc0df
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-12 18:32:07 +0530

    use faker to generate data in data store

--- src/testFixtures/kotlin/com/cj/dentalclinic/ClinicDataStore.kt
+++ src/testFixtures/kotlin/com/cj/dentalclinic/ClinicDataStore.kt
@@ -3,53 +3,142 @@ package com.cj.dentalclinic
 import com.cj.dentalclinic.entity.Clinic
 import com.cj.dentalclinic.entity.Doctor
 import com.cj.dentalclinic.entity.Treatment
+import io.github.serpro69.kfaker.Faker
 import java.util.*
+import kotlin.math.abs
+import kotlin.math.roundToInt
+
+const val DENTAL_CLINIC_SUFFIX = "Dental Clinic"
 
 class ClinicDataStore {
 
-  private val clinics = listOf(
-    Clinic(1, "Sharda Dental Clinic"),
-    Clinic(2, "Smart Dental Clinic"),
-    Clinic(3, "Sonal Dental Clinic")
-  )
+  val dcSuffix = DENTAL_CLINIC_SUFFIX
 
-  private val treatments = listOf(
-    Treatment(1, "Root Canal", 4500.00, clinics[0]),
-    Treatment(2, "Regular Checkup", 500.00, clinics[0]),
-    Treatment(3, "Regular Checkup", 100.00, clinics[1])
-  )
+  private val faker = Faker()
 
-  fun findAllClinics() = clinics
+  private val random = Random()
+
+  private val maxParentId = 4
+
+  val maxId = 9
+
+  val validId = random.nextInt(1, maxId)
+
+  val invalidId = random.nextInt(maxId, 100)
+
+  val parentIdWithData = random.nextInt(1, maxParentId)
+
+  val clinics = mutableListOf<Clinic>()
+
+  init {
+    for (id in 1..maxId) {
+      clinics.add(createClinic(id))
+    }
+  }
+
+  fun createClinic(id: Int?) = Clinic(id, "${faker.name.femaleFirstName()} $DENTAL_CLINIC_SUFFIX")
+
+  fun newClinic() = createClinic(null)
+
+  fun saveClinic(clinic: Clinic) = Clinic(randomIfNull(clinic.id), clinic.name)
 
   fun findClinicById(clinicId: Int): Optional<Clinic> = clinics
     .stream()
     .filter { it.id == clinicId }
     .findFirst()
 
-  fun newClinicId() = 4
+  fun clinicExistById(clinicId: Int): Boolean = clinics
+    .stream()
+    .anyMatch { it.id == clinicId }
 
-  fun newClinic() = Clinic(name = "Sky Dental Clinic")
+/**
+ * =====================================================================================================================
+ */
 
-  fun existingClinic() = clinics[0]
+  private val treatments = mutableListOf<Treatment>()
 
-  fun saveClinic(clinic: Clinic) = Clinic(newClinicId(), clinic.name)
+  val minTreatmentFee = 100.0
+
+  init {
+    for (id in 1..maxId) {
+      treatments.add(createTreatment(id))
+    }
+  }
+
+  fun createTreatment(id: Int?) =
+    Treatment(id = id, name = faker.restaurant.name(), fee = randomFee(), clinic = clinics[calculateParentIndex(id)])
+
+  private fun randomFee() = (random.nextDouble(minTreatmentFee, 100_000.0) * 100.0).roundToInt() / 100.0
+
+  private fun calculateParentIndex(id: Int?) = ((id ?: 1) % maxParentId)
+
+  private val doctors = mutableListOf<Doctor>()
+
+  fun newTreatment() = createTreatment(null)
+
+  fun saveTreatment(treatment: Treatment) =
+    Treatment(randomIfNull(treatment.id), treatment.name, treatment.fee, treatment.clinic)
+
+  private fun randomIfNull(id: Int?) = id ?: abs(random.nextInt())
 
   fun findAllTreatmentsByClinicId(clinicId: Int): List<Treatment> = treatments
     .stream()
-    .filter{ it.clinic.id == clinicId }
+    .filter { it.clinic.id == clinicId }
     .toList()
 
   fun findTreatmentById(treatmentId: Int): Optional<Treatment> = treatments
     .stream()
-    .filter{ it.id == treatmentId }
+    .filter { it.id == treatmentId }
     .findFirst()
 
-  fun newTreatmentId() = 4
+  fun treatmentExistById(treatmentId: Int): Boolean = treatments
+    .stream()
+    .anyMatch { it.id == treatmentId }
 
-  fun newTreatment() = Treatment(name = "Cleaning", fee = 500.00, clinic =  existingClinic())
+/**
+ * =====================================================================================================================
+ */
 
-  fun saveTreatment(treatment: Treatment) = Treatment(newTreatmentId() , treatment.name, treatment.fee, treatment.clinic)
+  init {
+    for (id in 1..maxId) {
+      doctors.add(createDoctor(id))
+    }
+  }
 
-  fun newDoctor() = Doctor(email = "atul@gmail.com", firstName = "Atul", lastName = "Kenjale", qualification = "BDS", clinic = clinics[0])
+  fun createDoctor(id: Int?): Doctor {
+    val firstName = faker.name.firstName()
+    val lastName = faker.name.lastName()
+    return Doctor(
+      id = id, email = faker.internet.email((lastName + firstName).lowercase()),
+      firstName = firstName, middleName = "", lastName = lastName,
+      qualification = faker.educator.degree(), clinic = clinics[calculateParentIndex(id)]
+    )
+  }
+
+  fun newDoctor() = createDoctor(null)
+
+  fun saveDoctor(doctor: Doctor) = Doctor(
+    randomIfNull(doctor.id),
+    doctor.email,
+    doctor.firstName,
+    doctor.middleName,
+    doctor.lastName,
+    doctor.qualification,
+    doctor.clinic
+  )
+
+  fun findAllDoctorsByClinicId(clinicId: Int): List<Doctor> = doctors
+    .stream()
+    .filter { it.clinic.id == clinicId }
+    .toList()
+
+  fun findDoctorById(doctorId: Int): Optional<Doctor> = doctors
+    .stream()
+    .filter { it.id == doctorId }
+    .findFirst()
+
+  fun doctorExistById(doctorId: Int): Boolean = doctors
+    .stream()
+    .anyMatch { it.id == doctorId }
 
 }
