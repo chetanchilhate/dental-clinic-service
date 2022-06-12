ce43ab4c9455de1f026068341f05e9e4911c26f7
Chetan Chilhate <chilhatechetan04@gmail.com>
2022-06-12 22:36:51 +0530

    add field validation annotation add error message mappings

--- src/main/kotlin/com/cj/dentalclinic/dto/DoctorDto.kt
+++ src/main/kotlin/com/cj/dentalclinic/dto/DoctorDto.kt
@@ -1,19 +1,25 @@
 package com.cj.dentalclinic.dto
 
 import com.cj.dentalclinic.entity.Doctor
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
 
 data class DoctorDto(
 
   val id: Int? = null,
 
+  @field:Email(message = "{error.doctor.email.invalid}")
   val email: String,
 
+  @field:NotBlank(message = "{doctor first name is mandatory}")
   val firstName: String,
 
   val middleName: String = "",
 
+  @field:NotBlank(message = "{doctor last name is mandatory}")
   val lastName: String,
 
+  @field:NotBlank(message = "{doctor qualification is mandatory}")
   val qualification: String
 
 ) {
